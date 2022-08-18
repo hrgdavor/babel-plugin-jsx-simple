f2bde09d0ce9b7156abcdfddea7926259103fa78
Davor Hrg <hrgdavor@gmail.com>
2022-08-18 14:00:06 +0200

    support fragments

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-
-module.exports = function (babel) {
+module.exports = function jsx_mi2(babel) {
   var t = babel.types
 
-  return {
-    inherits: require('babel-plugin-syntax-jsx'),
-    visitor: {
-      JSXNamespacedName (path) {
-        throw path.buildCodeFrameError(
-          'Namespaced tags/attributes are not supported. JSX is not XML.\n' +
-          'For attributes like xlink:href, use xlinkHref instead.'
-        )
-      },
-      JSXElement: {
-        exit (path, state) {
-          // turn tag into createElement call
-          var callExpr = buildElementCall(path.get('openingElement'), state)
-          if (path.node.children.length) {
-            // add children as 3rd+ arg
-            path.node.children.forEach(c=>callExpr.arguments.push(c));
-            // if you want to create an array instead, do it here
-          }
-          path.replaceWith(t.inherits(callExpr, path.node))
-        }
+  const jsxHandler = {
+    exit(path, state) {
+      // turn tag into createElement call
+      console.log('path', path)
+      var callExpr = buildElementCall(path.get('openingElement'), state)
+      if (path.node.children.length) {
+        // add children as 3rd+ arg
+        path.node.children.forEach(c => callExpr.arguments.push(c))
+        // if you want to create an array instead, do it here
       }
-    }
+      path.replaceWith(t.inherits(callExpr, path.node))
+    },
   }
 
-  function buildElementCall (path, state) {
-    if(state.opts.addArrow){
-      // extra option to add arrow around every child that is js expression 
-      replaceWithArrow(path.parent.children);
+  return {
+    //inherits: require('babel-plugin-syntax-jsx'),
+    visitor: {
+      JSXNamespacedName(path) {
+        throw path.buildCodeFrameError(
+          'Namespaced tags/attributes are not supported. JSX is not XML.\n' +
+            'For attributes like xlink:href, use xlinkHref instead.',
+        )
+      },
+      JSXFragment: jsxHandler,
+      JSXElement: jsxHandler,
+    },
+  }
+
+  function buildElementCall(path, state) {
+    if (state.opts.addArrow) {
+      // extra option to add arrow around every child that is js expression
+      replaceWithArrow(path.parent.children)
     }
 
     path.parent.children = t.react.buildChildren(path.parent)
 
-    var tagExpr = convertJSXIdentifier(path.node.name, path.node)
+    var tagExpr = convertJSXIdentifier(path.node ? path.node.name : t.NullLiteral(), path.node)
     var args = []
     var tagName
     if (t.isIdentifier(tagExpr)) {
@@ -43,28 +46,32 @@ module.exports = function (babel) {
       tagName = tagExpr.value
     }
 
-    if (t.react.isCompatTag(tagName)) { // starts with uppercase
+    if (t.react.isCompatTag(tagName)) {
+      // starts with uppercase
       args.push(t.stringLiteral(tagName))
     } else {
       args.push(tagExpr)
     }
 
-    var attribs = path.node.attributes
-    if (attribs.length) {
-      attribs = buildOpeningElementAttributes(attribs, state)
-    } else {
-      attribs = t.nullLiteral()
+    var attribs = t.NullLiteral()
+    if (path.node) {
+      attribs = path.node.attributes
+      if (attribs.length) {
+        attribs = buildOpeningElementAttributes(attribs, state)
+      } else {
+        attribs = t.nullLiteral()
+      }
     }
     args.push(attribs)
-
+    console.log('args', args)
     return t.callExpression(t.identifier('h'), args)
   }
 
-  function convertJSXIdentifier (node, parent) {
+  function convertJSXIdentifier(node, parent) {
     if (t.isJSXIdentifier(node)) {
       if (node.name === 'this' && t.isReferenced(node, parent)) {
         node = t.thisExpression()
-      } else{
+      } else {
         // Vue uses esutils here to confirm valid Identifier name
         // we do no such thing for our simple JSX transform
         node.type = 'Identifier'
@@ -72,18 +79,18 @@ module.exports = function (babel) {
     } else if (t.isJSXMemberExpression(node)) {
       node = t.memberExpression(
         convertJSXIdentifier(node.object, node),
-        convertJSXIdentifier(node.property, node)
+        convertJSXIdentifier(node.property, node),
       )
     }
     return node
   }
 
   /**
-   * Convert to object declaration by adding all 
+   * Convert to object declaration by adding all
    * props and spreads as they are found.
    */
 
-  function buildOpeningElementAttributes (attribs, state ) {
+  function buildOpeningElementAttributes(attribs, state) {
     var _props = []
 
     while (attribs.length) {
@@ -97,11 +104,11 @@ module.exports = function (babel) {
     }
 
     attribs = t.objectExpression(_props)
-    
+
     return attribs
   }
 
-  function convertAttribute (node, addArrow) {
+  function convertAttribute(node, addArrow) {
     var value = convertAttributeValue(node.value || t.booleanLiteral(true))
     if (t.isStringLiteral(value) && !t.isJSXExpressionContainer(node.value)) {
       value.value = value.value.replace(/\n\s+/g, ' ')
@@ -113,13 +120,13 @@ module.exports = function (babel) {
     }
 
     // extra option to add arrow around every attribute value that is js expression
-    if(addArrow && t.isJSXExpressionContainer(node.value)){
-      value = t.arrowFunctionExpression([],value)
+    if (addArrow && t.isJSXExpressionContainer(node.value)) {
+      value = t.arrowFunctionExpression([], value)
     }
     return t.inherits(t.objectProperty(node.name, value), node)
   }
 
-  function convertAttributeValue (node) {
+  function convertAttributeValue(node) {
     if (t.isJSXExpressionContainer(node)) {
       return node.expression
     } else {
@@ -127,10 +134,10 @@ module.exports = function (babel) {
     }
   }
 
-  function replaceWithArrow (ch) {
-    for(var i=0; i<ch.length; i++){
-      if(t.isJSXExpressionContainer(ch[i])){
-        ch[i].expression = t.arrowFunctionExpression([],ch[i].expression)
+  function replaceWithArrow(ch) {
+    for (var i = 0; i < ch.length; i++) {
+      if (t.isJSXExpressionContainer(ch[i])) {
+        ch[i].expression = t.arrowFunctionExpression([], ch[i].expression)
       }
     }
   }
